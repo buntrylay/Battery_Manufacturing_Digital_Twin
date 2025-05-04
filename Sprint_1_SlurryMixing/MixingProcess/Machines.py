c724bdae1730365b816304fb45e228a939043e63
Katsudon27 <152286098+Katsudon27@users.noreply.github.com>
2025-05-05 00:44:56 +1000

    Added methods to toggle on and off state for the machine

--- Sprint_1_SlurryMixing/MixingProcess/Machines.py
+++ Sprint_1_SlurryMixing/MixingProcess/Machines.py
@@ -4,3 +4,9 @@ class Machine(ABC):
     def __init__(self):
         self.is_on = False
         self.calculator = None
+
+    def turn_on(self):
+        self.is_on = True
+
+    def turn_off(self):
+        self.is_on = False
