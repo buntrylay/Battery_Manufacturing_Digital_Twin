ba7bfa397a0dbb2b9bfa4c6a1715f1376a7df2c9
Katsudon27 <152286098+Katsudon27@users.noreply.github.com>
2025-05-05 01:53:54 +1000

    Modified Factory class to reflect class diagram design

--- Sprint_1_SlurryMixing/MixingProcess/Factory.py
+++ Sprint_1_SlurryMixing/MixingProcess/Factory.py
@@ -3,8 +3,15 @@ from Machines import MixingMachine
 
 class Factory:
     def __init__(self):
+        self.machines = []
         self.slurry = Slurry(volume=200)
         self.mixer = MixingMachine(self.slurry)
 
+    def add_machine(self, machine):
+        self.machines.append(machine)
+
     def start_simulation(self):
-        self.mixer.run()
+        for machine in self.machines:
+            machine.turn_on()
+            machine.run()
+            machine.turn_off()
