9ffdbc05400e2e97e12b6b16792262dc899e9467
Katsudon27 <152286098+Katsudon27@users.noreply.github.com>
2025-05-05 00:16:49 +1000

    Added initialisation method for SlurryPropertyCalculator

--- Sprint_1_SlurryMixing/MixingProcess/SlurryPropertyCalculator.py
+++ Sprint_1_SlurryMixing/MixingProcess/SlurryPropertyCalculator.py
@@ -3,3 +3,6 @@ from Slurry import Slurry
 class SlurryPropertyCalculator:
     RHO = {"AM": 2.26, "CB": 1.8, "PVDF": 1.78, "NMP": 1.0}
     WEIGHTS = {"a": 0.9, "b": 2.5, "c": 0.3, "s": -0.5}
+
+    def __init__(self, slurry: Slurry):
+        self.slurry = slurry
