5889819aaa62e03e66d72176d347fd8a4642da6f
Katsudon27 <152286098+Katsudon27@users.noreply.github.com>
2025-05-05 00:19:08 +1000

    Added method for calculating viscosity

--- Sprint_1_SlurryMixing/MixingProcess/SlurryPropertyCalculator.py
+++ Sprint_1_SlurryMixing/MixingProcess/SlurryPropertyCalculator.py
@@ -12,3 +12,11 @@ class SlurryPropertyCalculator:
         total_mass = sum(m(c) for c in self.RHO)
         volume = self.slurry.get_total_volume()
         return total_mass / volume if volume else 0
+    
+    def calculate_viscosity(self, max_solid_fraction=0.63, intrinsic_viscosity=3):
+        total_volume = self.slurry.get_total_volume()
+        solid_volume = self.slurry.AM + self.slurry.CB + self.slurry.PVDF
+        phi = solid_volume / total_volume if total_volume else 0
+        if phi >= max_solid_fraction:
+            phi = max_solid_fraction - 0.001
+        return (1 - (phi / max_solid_fraction)) ** (-intrinsic_viscosity * max_solid_fraction) * 2
