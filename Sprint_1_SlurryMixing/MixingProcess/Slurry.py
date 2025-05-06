04b1c11bdf36ac1fcd97cbd6fb74d82c5b01478f
Min Khant Aung <minkhant.coding@gmail.com>
2025-05-06 21:32:05 +1000

    Removed Sprint_1_SlurryMixing folder from Min-Branch

---
+++ Sprint_1_SlurryMixing/MixingProcess/Slurry.py
@@ -0,0 +1,13 @@
+class Slurry:
+    def __init__(self, volume):
+        self.total_volume = volume
+        self.AM = 0
+        self.CB = 0
+        self.PVDF = 0
+        self.NMP = 0
+
+    def add(self, component, amount):
+        setattr(self, component, getattr(self, component) + amount)
+
+    def get_total_volume(self):
+        return self.AM + self.CB + self.PVDF + self.NMP
