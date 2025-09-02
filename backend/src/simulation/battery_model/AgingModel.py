21fa942cd444f2250d74f41a22085ad6f1b29d6d
giabaobui36 <100818088+giabaobui36@users.noreply.github.com>
2025-09-02 14:01:03 +1000

    create template for models

---
+++ backend/src/simulation/battery_model/AgingModel.py
@@ -0,0 +1,9 @@
+class AgingModel(BaseModel):
+    def __init__(self):
+        pass
+
+    def update_properties(self, machine_parameters: AgingParameters):
+        pass
+
+    def get_properties(self):
+        pass
---
+++ backend/src/simulation/battery_model/ElectrolyteFilling.py
@@ -0,0 +1,9 @@
+class ElectrolyteFillingModel(BaseModel):
+    def __init__(self):
+        pass
+
+    def update_properties(self, machine_parameters: ElectrolyteFillingParameters):
+        pass
+
+    def get_properties(self):
+        pass
