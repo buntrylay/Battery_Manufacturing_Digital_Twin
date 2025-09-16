aedb6bfc89faeda537ba151966690936f9ee01ea
Dwizzle29 <77puppy77@gmail.com>
2025-09-16 11:14:33 +1000

    Implemented React flow
    Important: Working with Dummy start button, need to implement the Websocket section and also combine websocket and flow socket context files.
    Mixing machine currently does not turn green, debugging needs to be done to FIx
    Added TODO sections for where noticiable updates will have to be completed

---
+++ frontend/src/components/AnimatedSVGEdge.js
@@ -0,0 +1,40 @@
+// src/components/AnimatedSVGEdge.js
+import React from "react";
+import { BaseEdge, getSmoothStepPath } from "@xyflow/react";
+
+export function AnimatedSVGEdge({
+  id,
+  sourceX,
+  sourceY,
+  targetX,
+  targetY,
+  sourcePosition,
+  targetPosition,
+}) {
+  const [edgePath] = getSmoothStepPath({
+    sourceX,
+    sourceY,
+    sourcePosition,
+    targetX,
+    targetY,
+    targetPosition,
+  });
+
+  return (
+    <>
+      {/* The base line of the edge */}
+      <BaseEdge id={id} path={edgePath} />
+
+      <circle r="4" fill="#007bff">
+        <animateMotion
+          dur="2s"
+          repeatCount="indefinite"
+          path={edgePath}
+          fill="freeze"
+        />
+      </circle>
+    </>
+  );
+}
+
+export default AnimatedSVGEdge;
