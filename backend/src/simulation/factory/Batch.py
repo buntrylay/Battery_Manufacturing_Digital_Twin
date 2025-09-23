595b3ea00e6b5fedaa2f9b9d2eeb6ea4fe8dae6c
giabaobui36 <100818088+giabaobui36@users.noreply.github.com>
2025-09-23 14:56:42 +1000

    add batch id to Batch

--- backend/src/simulation/factory/Batch.py
+++ backend/src/simulation/factory/Batch.py
@@ -2,7 +2,8 @@ from simulation.battery_model import MixingModel
 
 
 class Batch:
-    def __init__(self, id: str):
+    def __init__(self, batch_id: str):
+        self.batch_id = batch_id
         self.anode_line_model = MixingModel("Anode")
         self.cathode_line_model = MixingModel("Cathode")
         self.cell_line_model = None
