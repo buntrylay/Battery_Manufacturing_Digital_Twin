a432fa030df78a1d27405462cbfa8ba632814929
Min Khant Aung <minkhant.coding@gmail.com>
2025-10-10 12:28:37 +1100

    updated animation logic

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -4,6 +4,7 @@ import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import Navbar from "./components/Navbar";
 import Footer from "./components/Footer";
 import { WebSocketProvider } from "./contexts/WebSocketContext";
+import { FactorySimulationProvider } from "./contexts/FactorySimulationContext";
 
 // Styles
 import "./styles/App.css";
@@ -21,43 +22,45 @@ import RealTimeDataPage from "./pages/RealTimeDataPage";
 export default function App() {
   return (
     <WebSocketProvider>
-      <Router>
-        <div className="App">
-          <Navbar />
-          <main className="App-main">
-            <Routes>
-              <Route
-                path="/"
-                element={
-                  <FlowPageProvider>
-                    <FlowPage />
-                  </FlowPageProvider>
-                }
-              />
-              <Route
-                path="/flow"
-                element={
-                  <FlowPageProvider>
-                    <FlowPage />
-                  </FlowPageProvider>
-                }
-              />
-              <Route path="/logs" element={<LogsPage />} />
-              <Route
-                path="/realtime"
-                element={
-                  <FlowPageProvider>
-                    <RealTimeDataPage />{" "}
-                  </FlowPageProvider>
-                }
-              />
-              <Route path="/404" element={<h1>PAGE NOT FOUND</h1>} />
-              <Route path="*" element={<h1>PAGE NOT FOUND</h1>} />
-            </Routes>
-          </main>
-          <Footer />
-        </div>
-      </Router>
+      <FactorySimulationProvider>
+        <Router>
+          <div className="App">
+            <Navbar />
+            <main className="App-main">
+              <Routes>
+                <Route
+                  path="/"
+                  element={
+                    <FlowPageProvider>
+                      <FlowPage />
+                    </FlowPageProvider>
+                  }
+                />
+                <Route
+                  path="/flow"
+                  element={
+                    <FlowPageProvider>
+                      <FlowPage />
+                    </FlowPageProvider>
+                  }
+                />
+                <Route path="/logs" element={<LogsPage />} />
+                <Route
+                  path="/realtime"
+                  element={
+                    <FlowPageProvider>
+                      <RealTimeDataPage />
+                    </FlowPageProvider>
+                  }
+                />
+                <Route path="/404" element={<h1>PAGE NOT FOUND</h1>} />
+                <Route path="*" element={<h1>PAGE NOT FOUND</h1>} />
+              </Routes>
+            </main>
+            <Footer />
+          </div>
+        </Router>
+      </FactorySimulationProvider>
     </WebSocketProvider>
   );
 }
